2e868dd5d547b503348bb097062deb6a2b8f434e
Mehmet Ozkaya <mehmet.ozkaya@ericsson.com>
2018-08-10 19:29:34 +0300

    issue related developments

---
+++ UsageFramework/ConsoleUsage/GithubManager.cs
@@ -0,0 +1,63 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUsage
+{
+    internal class GithubManager
+    {
+        private readonly GitHubClient _client;
+
+        public GithubManager()
+        {
+            _client = new GitHubClient(new ProductHeaderValue("asdasdasdf"));
+            var basicAuth = new Credentials("mehmetozkaya", "Tatg.Wp1");
+            _client.Credentials = basicAuth;
+        }
+
+        
+        public async Task<User> GetUserInfo(string userName)
+        {
+            var user = await _client.User.Get(userName);
+            return user;
+        }
+
+        public async Task<IReadOnlyList<Issue>> GetIssues()
+        {
+            var issues = await _client.Issue.GetAllForCurrent();
+            return issues;
+        }
+
+        public async Task<IReadOnlyList<Issue>> GetIssuesForRepository(string userName, string repository)
+        {
+            var issuesForOctokit = await _client.Issue.GetAllForRepository(userName, repository);
+            return issuesForOctokit;
+        }
+
+        public void CreateIssue()
+        {
+
+        }
+
+
+        public void ApiInformation()
+        {
+            var apiInfo = _client.GetLastApiInfo();
+            var rateLimit = apiInfo?.RateLimit;
+            var howManyRequestsCanIMakePerHour = rateLimit?.Limit;
+            var howManyRequestsDoIHaveLeft = rateLimit?.Remaining;
+            var whenDoesTheLimitReset = rateLimit?.Reset; // UTC time
+        }
+
+        public Task<User> CurrentUser
+        {
+            get
+            {
+                return _client.User.Current();
+            }
+        }
+    }
+}
--- UsageFramework/ConsoleUsage/Program.cs
+++ UsageFramework/ConsoleUsage/Program.cs
@@ -13,27 +13,16 @@ namespace ConsoleUsage
         {
             try
             {
-                var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
-                var user = github.User.Get("mehmetozkaya");
-                Task.WaitAll(user);
-                var uu = user.Result;
-                Console.WriteLine(uu.Followers + " folks love the half ogre!");
+                var github = new GithubManager();
 
-                var client = new GitHubClient(new ProductHeaderValue("asdasdasdf"));
-                var basicAuth = new Credentials("mehmetozkaya", "Tatg.Wp1"); // NOTE: not real credentials
-                client.Credentials = basicAuth;
+                var userInfo = github.GetUserInfo("mehmetozkaya");
+                Task.WaitAll(userInfo);
 
-                var user2 = client.User.Get("shiftkey");
-                Task.WaitAll(user2);
+                var issues = github.GetIssues();
+                Task.WaitAll(issues);
 
-                var user3 = client.User.Current();
-                Task.WaitAll(user3);
-
-                var apiInfo = client.GetLastApiInfo();
-                var rateLimit = apiInfo?.RateLimit;
-                var howManyRequestsCanIMakePerHour = rateLimit?.Limit;
-                var howManyRequestsDoIHaveLeft = rateLimit?.Remaining;
-                var whenDoesTheLimitReset = rateLimit?.Reset; // UTC time
+                var issuesForRepository = github.GetIssuesForRepository("mehmetozkaya", "InterfaceActivityBuilder");
+                Task.WaitAll(issuesForRepository);
 
             }
             catch (Exception exception)
